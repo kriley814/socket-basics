4003ff8fd21a581c22b4aa376b39182b9ed33700
Kim Riley <kriley@hkmconsultingllc.com>
2016-04-08 10:05:46 -0400

    Show timestamp on UI

--- public/js/app.js
+++ public/js/app.js
@@ -5,11 +5,13 @@ socket.on('connect', function () {
 	console.log('Connected to socket.io server');
 });
 
+// modify function to 
 socket.on('message', function (message) {
+	var momentTimestamp = moment.utc(message.timestamp);
 	console.log('New message: ');
 	console.log(message.text);
 
-	jQuery('.messages').append('<p>' + message.text + '</p>');
+	jQuery('.messages').append('<p><strong>' + momentTimestamp.local().format('h:mm a') + ': </strong>' + message.text + '</p>');
 });
 
 // Handles submitting od new message
