4e6d7fb646cc1e1c74678631da631a3a2d55c4c7
Kim Riley <kriley@hkmconsultingllc.com>
2016-04-11 12:05:41 -0400

    Add name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,22 @@
-
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
 var socket = io();
+
+console.log(name + ' wants to join ' + room);
 
 socket.on('connect', function () {
 	console.log('Connected to socket.io server');
 });
-
-// modify function to 
+ 
 socket.on('message', function (message) {
 	var momentTimestamp = moment.utc(message.timestamp);
+	var $message = jQuery('.messages');
+
 	console.log('New message: ');
 	console.log(message.text);
 
-	jQuery('.messages').append('<p><strong>' + momentTimestamp.local().format('h:mm a') + ': </strong>' + message.text + '</p>');
+	$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
+	$message.append('<p>' + message.text + '</p>');
 });
 
 // Handles submitting od new message
@@ -19,12 +24,52 @@ var $form = jQuery('#message-form');
 
 $form.on('submit', function (event) {
 	event.preventDefault(); // handle the form submission on our own - no refresh needed
+	
 	var $message = $form.find('input[name=message]');
+	
 	socket.emit('message', {
+		name: name,
 		text: $message.val()
 	});
 
 	// erase contents of input
-	$message.val("");
+	$message.val('');
 
 });
+
+// var name = getQueryVariable('name') || 'Anonymous';
+// var room = getQueryVariable('room');
+// var socket = io();
+
+// console.log(name + ' wants to join ' + room);
+
+// socket.on('connect', function () {
+// 	console.log('Conncted to socket.io server!');
+// });
+
+// socket.on('message', function (message) {
+// 	var momentTimestamp = moment.utc(message.timestamp);
+// 	var $message = jQuery('.messages');
+
+// 	console.log('New message:');
+// 	console.log(message.text);
+
+// 	$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
+// 	$message.append('<p>' + message.text + '</p>');
+// });
+
+// // Handles submitting of new message
+// var $form = jQuery('#message-form');
+
+// $form.on('submit', function (event) {
+// 	event.preventDefault();
+
+// 	var $message = $form.find('input[name=message]');
+
+// 	socket.emit('message', {
+// 		name: name,
+// 		text: $message.val()
+// 	});
+
+// 	$message.val('');
+// });
--- server.js
+++ server.js
@@ -1,25 +1,58 @@
+// var PORT = process.env.PORT || 3000;
+// var moment = require('moment');
+// var express = require('express');
+// var app = express();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+
+// app.use(express.static(__dirname + '/public'));
+
+// io.on('connection', function (socket) {
+// 	console.log('User connected via socket.io!');
+
+// 	socket.on('message', function (message) {
+// 		console.log('Message received: ' + message.text);
+
+// 		message.timestamp = moment().valueOf();
+// 		io.emit('message', message);
+// 	});
+
+// 	socket.emit('message', {
+// 		name: 'System',
+// 		text: 'Welcome to the chat application!',
+// 		timestamp: moment.valueOf()
+// 	});
+// });
+
+// http.listen(PORT, function () {
+// 	console.log('Server started!');
+// });
+
 var PORT = process.env.PORT || 3000;
+var moment = require('moment');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var moment = require('moment');
-
 
 app.use(express.static(__dirname + '/public'));
+
 io.on('connection', function (socket) {
 	console.log('User connected via socket.io!');
 
 	socket.on('message', function (message) {
 		console.log('Message received: ' + message.text);
 
-message.timestamp = moment().valueOf();
+		message.timestamp = moment().valueOf();
 		io.emit('message', message);
 	});
 
+	// timestamp property - JavaScript timestamp (milliseconds)
+
 	socket.emit('message', {
+		name: 'System',
 		text: 'Welcome to the chat application!',
-		timestamp: moment.valueOf()
+		timestamp: moment().valueOf()
 	});
 });
 
