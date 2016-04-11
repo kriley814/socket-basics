c11cc0f61463a2a3a06d7e98e70a1f95ce5f022c
Kim Riley <kriley@hkmconsultingllc.com>
2016-04-11 13:13:07 -0400

    Add support for chat rooms

--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,15 @@ var socket = io();
 
 console.log(name + ' wants to join ' + room);
 
+// update h1 tag
+jQuery('.room-title').text(room);
+
 socket.on('connect', function () {
 	console.log('Connected to socket.io server');
+	socket.emit('joinRoom', {
+		name: name,
+		room: room
+	});
 });
  
 socket.on('message', function (message) {
@@ -37,39 +44,3 @@ $form.on('submit', function (event) {
 
 });
 
-// var name = getQueryVariable('name') || 'Anonymous';
-// var room = getQueryVariable('room');
-// var socket = io();
-
-// console.log(name + ' wants to join ' + room);
-
-// socket.on('connect', function () {
-// 	console.log('Conncted to socket.io server!');
-// });
-
-// socket.on('message', function (message) {
-// 	var momentTimestamp = moment.utc(message.timestamp);
-// 	var $message = jQuery('.messages');
-
-// 	console.log('New message:');
-// 	console.log(message.text);
-
-// 	$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
-// 	$message.append('<p>' + message.text + '</p>');
-// });
-
-// // Handles submitting of new message
-// var $form = jQuery('#message-form');
-
-// $form.on('submit', function (event) {
-// 	event.preventDefault();
-
-// 	var $message = $form.find('input[name=message]');
-
-// 	socket.emit('message', {
-// 		name: name,
-// 		text: $message.val()
-// 	});
-
-// 	$message.val('');
-// });
--- server.js
+++ server.js
@@ -1,33 +1,3 @@
-// var PORT = process.env.PORT || 3000;
-// var moment = require('moment');
-// var express = require('express');
-// var app = express();
-// var http = require('http').Server(app);
-// var io = require('socket.io')(http);
-
-// app.use(express.static(__dirname + '/public'));
-
-// io.on('connection', function (socket) {
-// 	console.log('User connected via socket.io!');
-
-// 	socket.on('message', function (message) {
-// 		console.log('Message received: ' + message.text);
-
-// 		message.timestamp = moment().valueOf();
-// 		io.emit('message', message);
-// 	});
-
-// 	socket.emit('message', {
-// 		name: 'System',
-// 		text: 'Welcome to the chat application!',
-// 		timestamp: moment.valueOf()
-// 	});
-// });
-
-// http.listen(PORT, function () {
-// 	console.log('Server started!');
-// });
-
 var PORT = process.env.PORT || 3000;
 var moment = require('moment');
 var express = require('express');
@@ -37,14 +7,26 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
+var clientInfo = {};
+
 io.on('connection', function (socket) {
 	console.log('User connected via socket.io!');
+
+	socket.on('joinRoom', function (req) {
+		clientInfo[socket.id] = req;
+		socket.join(req.room);
+		socket.broadcast.to(req.room).emit('message', {
+			name: 'System',
+			text: req.name + ' has joined!',
+			timestamp: moment().valueOf()
+		});
+	});
 
 	socket.on('message', function (message) {
 		console.log('Message received: ' + message.text);
 
 		message.timestamp = moment().valueOf();
-		io.emit('message', message);
+		io.to(clientInfo[socket.id].room).emit('message', message);
 	});
 
 	// timestamp property - JavaScript timestamp (milliseconds)
